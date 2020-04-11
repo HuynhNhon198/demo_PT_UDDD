fc57c3ecec9c51f18c33ef9ecf90bea631314b56
TruongXuanHoi <truongxuanhoi2710@gmail.com>
2020-04-11 22:19:19 +0700

    hoi

--- src/app/data/lythuyet.ts
+++ src/app/data/lythuyet.ts
@@ -1,25 +1,112 @@
+import { url } from 'inspector';
+
 const getLyThuyet = () => {
     return [
         {
-            img: 'https://image.shutterstock.com/image-vector/math-symbols-vector-icon-260nw-632470322.jpg',
-            title: 'Chương 1',
-            desc: 'đây là chương 1',
+            img: 'assets/chuong 1/chuong1.JPG',
+            title: 'CHƯƠNG 1',
+            desc: 'CÁC SỐ ĐẾM ĐẾN 10. HÌNH VUÔNG, HÌNH TRÒN, HÌNH TAM GIÁC',
             details: [
-                'assets/chuong/chuong1-1.png',
-                'assets/chuong/chuong1-2.png'
+                'assets/chuong 1/chuong1-1.JPG',
+                'assets/chuong 1/chuong1-2.JPG',
+                'assets/chuong 1/chuong1-3.JPG',
+                'assets/chuong 1/chuong1-4.JPG',
+                'assets/chuong 1/chuong1-5.JPG',
+                'assets/chuong 1/chuong1-6.JPG',
+                'assets/chuong 1/chuong1-7.JPG',
+                'assets/chuong 1/chuong1-8.JPG',
+                'assets/chuong 1/chuong1-9.JPG',
+                'assets/chuong 1/chuong1-10.JPG',
+                'assets/chuong 1/chuong1-11.JPG',
+                'assets/chuong 1/chuong1-12.JPG',
+                'assets/chuong 1/chuong1-13.JPG',
+                'assets/chuong 1/chuong1-14.JPG',
             ]
         },
         {
-            img: 'https://cdn1.iconfinder.com/data/icons/banking-and-finance-2-4/128/89-512.png',
-            title: 'Chương 2',
-            desc: 'đây là chương 2',
-            details: []
+            img: 'assets/chuong 2/chuong2.JPG',
+            title: 'CHƯƠNG 2',
+            desc: 'PHÉP CỘNG, PHÉP TRỪ TRONG PHẠM VI 10',
+            details: [
+                'assets/chuong 2/chuong2-1.JPG',
+                'assets/chuong 2/chuong2-2.JPG',
+                'assets/chuong 2/chuong2-3.JPG',
+                'assets/chuong 2/chuong2-4.JPG',
+                'assets/chuong 2/chuong2-5.JPG',
+                'assets/chuong 2/chuong2-6.JPG',
+                'assets/chuong 2/chuong2-7.JPG',
+                'assets/chuong 2/chuong2-8.JPG',
+                'assets/chuong 2/chuong2-9.JPG',
+                'assets/chuong 2/chuong2-10.JPG',
+                'assets/chuong 2/chuong2-11.JPG',
+                'assets/chuong 2/chuong2-12.JPG',
+                'assets/chuong 2/chuong2-13.JPG',
+                'assets/chuong 2/chuong2-14.JPG',
+                'assets/chuong 2/chuong2-15.JPG',
+                'assets/chuong 2/chuong2-16.JPG',
+                'assets/chuong 2/chuong2-17.JPG',
+                'assets/chuong 2/chuong2-18.JPG',
+                'assets/chuong 2/chuong2-19.JPG',
+               
+
+            ]
         },
         {
-            img: 'https://cdn2.iconfinder.com/data/icons/circle-icons-1/64/calculator-512.png',
-            title: 'Chương 3',
-            desc: 'đây là chương 3',
-            details: []
+            img: 'assets/chuong 3/chuong3.JPG',
+            title: 'CHƯƠNG 3',
+            desc: 'CÁC SỐ TRONG PHẠM VI 100. ĐO ĐỘ DÀI, GIẢI BÀI TOÁN',
+            details: [
+                'assets/chuong 3/chuong3-1.JPG',
+                'assets/chuong 3/chuong3-2.JPG',
+                'assets/chuong 3/chuong3-3.JPG',
+                'assets/chuong 3/chuong3-4.JPG',
+                'assets/chuong 3/chuong3-5.JPG',
+                'assets/chuong 3/chuong3-6.JPG',
+                'assets/chuong 3/chuong3-7.JPG',
+                'assets/chuong 3/chuong3-8.JPG',
+                'assets/chuong 3/chuong3-9.JPG',
+                'assets/chuong 3/chuong3-10.JPG',
+                'assets/chuong 3/chuong3-11.JPG',
+                'assets/chuong 3/chuong3-12.JPG',
+                'assets/chuong 3/chuong3-13.JPG',
+                'assets/chuong 3/chuong3-14.JPG',
+                'assets/chuong 3/chuong3-15.JPG',
+                'assets/chuong 3/chuong3-16.JPG',
+                'assets/chuong 3/chuong3-17.JPG',
+                'assets/chuong 3/chuong3-18.JPG',
+                'assets/chuong 3/chuong3-19.JPG',
+                'assets/chuong 3/chuong3-20.JPG',
+                'assets/chuong 3/chuong3-21.JPG',
+                'assets/chuong 3/chuong3-22.JPG',
+                'assets/chuong 3/chuong3-23.JPG',
+                'assets/chuong 3/chuong3-24.JPG',
+                'assets/chuong 3/chuong3-25.JPG',
+                'assets/chuong 3/chuong3-26.JPG',
+                'assets/chuong 3/chuong3-27.JPG',
+                'assets/chuong 3/chuong3-28.JPG',
+                
+            ]
+        },
+        {
+            img: 'assets/chuong 4/chuong4.JPG',
+            title: 'CHƯƠNG 4',
+            desc: 'PHÉP CỘNG, PHÉP TRỪ TRONG PHẠM VI 100. ĐO THỜI GIAN',
+            details: [
+                'assets/chuong 4/chuong4-1.JPG',
+                'assets/chuong 4/chuong4-2.JPG',
+                'assets/chuong 4/chuong4-3.JPG',
+                'assets/chuong 4/chuong4-4.JPG',
+                'assets/chuong 4/chuong4-5.JPG',
+                'assets/chuong 4/chuong4-6.JPG',
+                'assets/chuong 4/chuong4-7.JPG',
+                'assets/chuong 4/chuong4-8.JPG',
+                'assets/chuong 4/chuong4-9.JPG',
+                'assets/chuong 4/chuong4-10.JPG',
+                'assets/chuong 4/chuong4-11.JPG',
+                'assets/chuong 4/chuong4-12.JPG',
+                'assets/chuong 4/chuong4-13.JPG',
+                'assets/chuong 4/chuong4-14.JPG',
+            ]
         }
     ];
 };
